be2ae898c976080606f82060ab2f79a3e7565b6b
Brandon Murry <murryb@amazon.com>
2016-12-01 14:34:27 -0800

    removed example

--- js.js
+++ js.js
@@ -1,67 +1,9 @@
 (function(){
-  var obj1 = {
-    properties: {
-      selectionName: null,
-      price: {
-        format: 'fractional',
-        odds: null
-      },
-      status: 'A',
-      display: {
-        displayed: 'Y',
-        order: null
-      },
-      selectionScore: {
-        home: null,
-        away: null
-      },
-      band: {
-        lower: 5,
-        upper: 10
-      },
-      channels: 'ZWIP'
-    },
-    result: null
-  };
-
-  var obj2 = {
-    properties: {
-      selectionName: null,
-      price: {
-        format: 'fractional',
-        odds: null
-      },
-      status: 'A',
-      display: {
-        displayed: 'Y',
-        order: null
-      },
-      selectionScore: {
-        home: null,
-        away: null
-      },
-      band: {
-        lower: null,
-        upper: null
-      },
-      channels: 'ZWIP'
-    },
-    result: null
-  };
-
-  var lodash = _.noConflict();
-  var underscore = _.noConflict()
+  benchmark(function(){
+    return 'hello';
+  }, 'Benchmark 1');
 
   benchmark(function(){
-    JSON.stringify(obj1) === JSON.stringify(obj2);
-  }, 'JSON.Stringify');
-
-  benchmark(function(){
-    lodash.isEqual(obj1, obj2);
-  }, 'Lodash');
-
-  benchmark(function(){
-    underscore.isEqual(obj1, obj2);
-  }, 'Underscore');
-
+    return 'world';
+  }, 'Benchmark 2');
 }());
