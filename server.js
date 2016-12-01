97bb37a1e428f850256bf8afc07082141e0e9db1
Brandon Murry <murryb@amazon.com>
2016-12-01 13:57:22 -0800

    server only

---
+++ server.js
@@ -0,0 +1,18 @@
+/* eslint no-console: 0 */
+'use strict';
+
+const path = require('path');
+const express = require('express');
+const port = process.env.PORT || '9000';
+const app = express();
+
+
+// main page
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, '0.0.0.0', function onStart(err) {
+  if (err) console.log(err);
+  console.info('==> Server started on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+});
